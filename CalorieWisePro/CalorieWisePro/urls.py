9ed3474b38f063a95b26b4b65beaea1b4ee81e40
Samhan-6 <msmsamhan844@gmail.com>
2023-10-13 23:41:34 +0530

    website fully working

--- CalorieWisePro/CalorieWisePro/urls.py
+++ CalorieWisePro/CalorieWisePro/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 import contact
 
 urlpatterns = [
